6056e9fc44503f19d697f3c85b259c192b0e570e
pedrohsn <pedrodti2017@gmail.com>
2022-04-14 13:53:12 -0300

    request no server, com tratamento. Sem framework

--- index.js
+++ index.js
@@ -2,29 +2,46 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');//file system
 
-
-
-http.createServer((request, response) => {
-
-  let path = url.parse(request.url).pathname;
-  
-  if(path == "" || path == "/"){//se o c caminho for vazio ou / ele vai direito pro index
-    path = "/index.html";
-  }
+function handleFile(req, res, callback){
+  let path = url.parse(req.url).pathname;
   let fileName = "." + path;
 
-  fs.readFile(fileName, (err, data) =>{
+  fs.readFile(fileName, (err, data) =>{//lê o arquivo com o caminho de filename
     if(err) { // se não encontrar o arquivo 
-      response.writeHead(404, {"Content-Type":"text/html;charset=UTF-8"}); //text/html text/plain application/json
-      response.end("<h1>Página Não encontrada</h1>")//msg na tela
+          if(callback){
+           if(!callback(req, res)){
+             
+              res.writeHead(404, {"Content-Type":"text/html;charset=UTF-8"}); //text/html text/plain application/json
+              res.end("<h1>Página Não encontrada</h1>")//msg na tela
+           }
+          }
+        
+          
     }else{
-      response.writeHead(200, {"Content-Type":"text/html"})
-      response.write(data);//vai mostrar oq tem no arquivo
-      response.end();
+          res.writeHead(200, {"Content-Type":"text/html"})
+          res.write(data);//vai mostrar oq tem no arquivo
+          res.end();
     }
   })
+}
 
-}).listen(3000,(err) => {
+function handleRequest(req, res) {
+  let path = url.parse(req.url).pathname;
+
+  if(path == "/teste"){
+    res.end("Teste");
+    return true;
+  }
+
+  return false;
+}
+
+http.createServer((request, response) => {
+  
+    handleFile(request, response, handleRequest);
+
+
+}).listen(3000,(err) => { //escolhe a porta
   if(err){
     console.log(err) 
   }else {
